966e41cfac5871896e7d8674555463974700ce86
Atharva Dhadke <atharvadhadke2002@gmail.com>
2023-01-06 19:13:39 +0530

    Implemented API fetching and performed destrcuturing

--- src/components/Temp.js
+++ src/components/Temp.js
@@ -1,8 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './style.css'
 
 const Temp = () => {
-  return (
+    const [searchValue, setsearchValue] = useState("Mumbai");
+    const [tempInfo, setTempInfo] = useState({});
+    
+    const getWeatherInfo = async ()=>{
+        try {
+            // let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&units=metric&appid=60ae4433d51decb796f289c06c32072a`;
+
+            let res = await fetch(url);
+            let data= await res.json();
+
+            const { temp, humidity,pressure} = data.main;
+            const {main:weathermood} = data.weather[0];
+            const {name } = data;
+            const {speed} = data.wind;
+            const {country, sunset} = data.sys;
+            // console.log(data);
+
+            const myNewWeatherInfo = {
+                temp,
+                humidity,
+                pressure,
+                weathermood,
+                name,
+                speed,
+                country,
+                sunset,
+            };
+
+            setTempInfo(myNewWeatherInfo);
+
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    useEffect(()=>{
+        getWeatherInfo();
+    },[]);
+ 
+    return (
     <div>
       <div className="wrap">
         <div className="search">
@@ -10,8 +49,10 @@ const Temp = () => {
             placeholder="search"
             autofocus id = "search"
             className="searchTerm"  
+            value = {searchValue}
+            onChange = {(e)=> setsearchValue(e.target.value)}
             />
-            <button type="button" className="searchButton">
+            <button type="button" className="searchButton" onClick={getWeatherInfo}>
                 Search
             </button>
         </div>
